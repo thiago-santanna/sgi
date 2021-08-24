5abc782865f90da72867cbfd0601214bfe82b62a
xthiagosant <uzmaki_thiago@outlook.com>
2021-08-24 11:23:46 -0300

    criado um exemplo de alteração parcial com patch no estado

--- src/main/java/com/tsswebapps/sgi/api/controller/v1/EstadoController.java
+++ src/main/java/com/tsswebapps/sgi/api/controller/v1/EstadoController.java
@@ -1,13 +1,18 @@
 package com.tsswebapps.sgi.api.controller.v1;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tsswebapps.sgi.domain.exception.EstadoNaoEncontradoException;
 import com.tsswebapps.sgi.domain.exception.NegocioException;
 import com.tsswebapps.sgi.domain.model.Estado;
@@ -51,6 +57,28 @@ public class EstadoController {
 		Estado estadoAtual = service.buscarOuFalhar(id);
 		BeanUtils.copyProperties(estadoAlterado, estadoAtual, "id", "pais");
 		return service.salvar(estadoAtual);
+	}
+	
+	@PatchMapping("/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public Estado atualizarParcial(@PathVariable Long id, @RequestBody Map<String, Object> campos){
+		Estado estadoAtual = service.buscarOuFalhar(id);
+		
+		merge(campos, estadoAtual);
+		
+		return atualizar(id, estadoAtual);
+	}
+
+	private void merge(Map<String, Object> campos, Estado estadoDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Estado estadoOriginal = objectMapper.convertValue(campos, Estado.class);
+		
+		campos.forEach((chave, valor) -> {
+			Field field = ReflectionUtils.findField(Estado.class, chave);
+			field.setAccessible(true);					
+			Object novoValor = ReflectionUtils.getField(field, estadoOriginal);			
+			ReflectionUtils.setField(field, estadoDestino, novoValor);
+		});
 	}
 	
 	@DeleteMapping("/{id}")
